2b5b22b0adc25da09cfb8f6e86f32782b0baa52c
Collins243-hue <collobarasa12@gmail.com>
2026-05-28 13:26:47 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+ public class Main{
+    public static void main (String[] args) {
+        System.out.println("COLLINS BARASA");
+        System.out.println("KCA UNIVERSITY");
+        System.out.println("JAVA PROGRAMMING");
+    }
+
+}
